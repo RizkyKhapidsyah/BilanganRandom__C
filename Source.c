f1c9f1ad8144f441cd09ccda0bc1ab0ff9749dfd
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-03-07 21:59:34 +0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,18 @@
+/* Pemakaian standard library untuk membangkitkan bilangan random */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+	const int MIN = 1;
+	const int MAX = 100;
+	
+	/* Program akan membangkitkan 5 bilangan random antar MIN sampai MAX  */
+	
+	for (int i = 0; i < 5; i++)
+		printf(" %d ", rand() % MAX + MIN);
+
+	_getch();
+	return 0;
+}
